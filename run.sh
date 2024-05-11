49d4d8fefc3e16edf1534bf83d13ebb0a7911820
come_ill_foo <rayveleaveme34@inbox.ru>
2024-05-11 18:59:06 +0100

    refactor: satisfy TAP 3 style

--- run.sh
+++ run.sh
@@ -1,41 +1,61 @@
 #!/usr/bin/env bash
 
+# @brief return code on failure
+RC_NOT_OK=1
+
+# @brief return code if not satisfied with environment
+RC_SKIP=4
+
 # @brief namespace id
 NS=ns-$$
 
 # @brief random packet loss percentage
 loss_random="$1"
 
-set -euo pipefail
-
 cleanup() {
-    echo "Deleting network namespace $NS"
+    echo "## Deleting network namespace $NS..."
     ip netns delete $NS
 }
 
+require_command() {
+    if ! command -v "$1" >/dev/null 2>&1; then
+        return 1
+    fi
+    return 0
+}
+
+set -euo pipefail
+
+
+if ! require_command python3; then
+    echo '# not ok: python3 not found [SKIP]'
+    exit $RC_SKIP
+fi
 
 if ! ip netns add $NS; then
-    echo 'Failed to add network namespace'
-    exit 1
+    echo '# not ok: failed to add network namespace'
+    exit $RC_NOT_OK
 fi
-echo "Added network namespace $NS"
-trap cleanup EXIT 1 2 3 6
+echo "## Added network namespace $NS"
+trap cleanup 1 2 3 6
 
 if [ -n "${loss_random}" ]; then
-    echo "Setting random loss to ${loss_random}..."
+    echo "## Setting random loss to ${loss_random}..."
     if ! ip netns exec $NS tc qdisc add dev lo root netem loss random "${loss_random}"; then
-        echo "Failed to set random loss to ${loss_random}"
         cleanup
-        exit 1
+        echo "# not ok: failed to set random loss to ${loss_random}"
+        exit $RC_NOT_OK
     fi
-    echo 'Check lo settings:'
+    echo '## Check lo settings:'
     ip netns exec $NS tc qdisc show dev lo
 fi
 
 if ! ip netns exec $NS ip link set dev lo up; then
-    echo 'Failed to set lo up'
     cleanup
-    exit 1
+    echo '# not ok: failed to set lo up'
+    exit $RC_NOT_OK
 fi
-echo 'Set up lo'
+echo '## Set up lo'
 
+cleanup
+echo '# ok: test finished successfully'
